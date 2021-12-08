f1df313381edebccbda68040c026e9d62859de27
Peter Conley <peterdavidconley@gmail.com>
2021-12-08 16:10:11 -0500

    created initial state with Bitcoin data

--- src/reducers/reducers.js
+++ src/reducers/reducers.js
@@ -1,5 +1,26 @@
 const initialState = {
-    price: '',
+    
+"coin": {
+        "id": "bitcoin",
+        "icon": "https://static.coinstats.app/coins/Bitcoin6l39t.png",
+        "name": "Bitcoin",
+        "symbol": "BTC",
+        "rank": 1,
+        "price": 50727.330274100466,
+        "priceBtc": 1,
+        "volume": 36721400765.30378,
+        "marketCap": 958514769008.4764,
+        "availableSupply": 18895431,
+        "totalSupply": 21000000,
+        "priceChange1h": 0.58,
+        "priceChange1d": 0.64,
+        "priceChange1w": -10.67,
+        "websiteUrl": "http://www.bitcoin.org",
+        "twitterUrl": "https://twitter.com/bitcoin",
+        "exp": ["https://blockchair.com/bitcoin/", "https://btc.com/", "https://btc.tokenview.com/"]
+        },
+  "isFetching": false,
+  "error": ''
  }
 
 export const reducer = (state = initialState, action) => {
